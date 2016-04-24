66376b14f3e6dc27fe3916f370a7c6aef944a1dc
Kevin Yauris <kyauris@gmail.com>
2016-04-25 00:12:18 +0700

    fixing some frontend

--- aplikasi/resources/views/app_template.blade.php
+++ aplikasi/resources/views/app_template.blade.php
@@ -91,16 +91,6 @@
         <ul class="nav navbar-nav navbar-right">
           <li class="dropdown">
             <a href="#" data-toggle="dropdown" class="dropdown-toggle">
-              <i class="icon-bdg_alert text14"></i>
-              <span class="visible-xs-inline">Notifikasi</span>
-              <span class="badge badge-sm up bg-danger pull-right-xs"></span>
-            </a>
-            <!-- dropdown -->
-            @yield('notifikasi')
-            <!-- / dropdown -->
-          </li>
-          <li class="dropdown">
-            <a href="#" data-toggle="dropdown" class="dropdown-toggle">
               <i class="icon-bdg_search text14"></i>              
             </a>
             <!-- dropdown -->
@@ -153,89 +143,7 @@
   </header>
   <!-- / header -->
 
-  <aside id="aside" class="app-aside hidden-xs bg-dark">
-      <div class="aside-wrap">
-        <div class="navi-wrap">
-          <!-- user -->
-          <div class="clearfix hidden-xs text-center hide" id="aside-user">
-            <div class="dropdown wrapper">
-              <a href="app.page.profile">
-                <span class="thumb-lg w-auto-folded avatar m-t-sm">
-                  <img src="img/01.jpg" class="img-full" alt="...">
-                </span>
-              </a>
-              <a href="#" data-toggle="dropdown" class="dropdown-toggle hidden-folded">
-                <span class="clear">
-                  <span class="block m-t-sm">
-                    <strong class="font-bold text-lt">John.Smith</strong> 
-                    <b class="caret"></b>
-                  </span>
-                  <span class="text-muted text-xs block">Art Director</span>
-                </span>
-              </a>
-              <!-- dropdown -->
-              <ul class="dropdown-menu animated fadeInRight w hidden-folded">
-                <li class="wrapper b-b m-b-sm bg-info m-t-n-xs">
-                  <span class="arrow top hidden-folded arrow-info"></span>
-                  <div>
-                    <p>300mb of 500mb used</p>
-                  </div>
-                  <div class="progress progress-xs m-b-none dker">
-                    <div class="progress-bar bg-white" data-toggle="tooltip" data-original-title="50%" style="width: 50%"></div>
-                  </div>
-                </li>
-                <li>
-                  <a href>Settings</a>
-                </li>
-                <li>
-                  <a href="page_profile.html">Profile</a>
-                </li>
-                <li>
-                  <a href>
-                    <span class="badge bg-danger pull-right">3</span>
-                    Notifications
-                  </a>
-                </li>
-                <li class="divider"></li>
-                <li>
-                  <a href="page_signin.html">Logout</a>
-                </li>
-              </ul>
-              <!-- / dropdown -->
-            </div>
-            <div class="line dk hidden-folded"></div>
-          </div>
-          <!-- / user -->
-
-         <!-- nav -->
-          <nav ui-nav class="navi clearfix">
-            <ul class="nav">
-              <li class="hidden-folded m-t text-dark-grey text-xs padder-md padder-v-sm">
-                <span>Navigation</span>
-              </li>
-              <li>
-                <a href="table_static.html" class="auto">
-                  <i class="icon-bdg_dashboard icon-grey"></i>
-                  <span class="font-bold">Dashboard</span>
-                </a>
-              </li>
-              
-              <li class="line1 dg"></li>
-
-              <li class="hidden-folded text-dark-grey text-xs padder-md padder-v-sm">
-                <span>Components</span>
-              </li>
-        
-            </ul>
-          </nav>
-          <!-- nav -->
-
-
-
-        </div>
-      </div>
-  </aside>
-  <!-- / aside -->
+ 
 
 
 <!-- content -->
@@ -247,10 +155,6 @@
 
           <div class="bg-light lter">    
               <ul class="breadcrumb bg-grey-breadcrumb m-b-none">
-                <li><a href="#" class="btn no-shadow" ui-toggle-class="app-aside-folded" target=".app">
-                  <i class="icon-bdg_expand1 text"></i>
-                  <i class="icon-bdg_expand2 text-active"></i>
-                </a>   </li>
                 <li><a href="{{URL::to('/')}}">Home</a></li>
                 @yield('appside')               
               </ul>
@@ -286,10 +190,6 @@
 
   <!-- footer -->
   <footer id="footer" class="app-footer" role="footer">
-    <div class="wrapper-md padder-lg b-t bg-light">
-      <span class="pull-right">&copy; Copyright Bandung <a href ui-scroll="app" class="m-l-sm text-muted"><i class="icon-bdg_arrow11"></i></a></span>
-     Made with <i class="text-danger fa fa-heart"></i> in Bandung
-    </div>
   </footer>
   <!-- / footer -->
 </div>
